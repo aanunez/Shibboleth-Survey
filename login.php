e6be0bd61082765da48a70c108405e533f6707aa
adam nunez <adam.a.nunez@gmail.com>
2024-07-10 18:07:36 -0700

    Added action tag and resolved minor bugs

--- login.php
+++ login.php
@@ -1,11 +1,17 @@
 <?php
 // Being able to access this page means the user has logged in
-// http://localhost.edu/redcap_v99.9.99/ExternalModules/?prefix=SurveyAuth&page=login&pid=16&s=1234567890
+// http://localhost.edu/redcap_v99.9.99/ExternalModules/?prefix=shibboleth_survey_auth&page=login&pid=16&s=1234567890
 $survey = $_GET["s"];
 $time = time();
 $item = $module->getSystemSetting("user-item");
 $user = $_SERVER[empty($item) ? $module->defaultItem : $item];
-$hash = $module->makeHash($survey, $user, $time);
-$base = "http" . (empty($_SERVER["HTTPS"]) ? "" : "s") . "://". $_SERVER["HTTP_HOST"];
 
-header("Location: $base/surveys/?s=$survey&auth=$hash" );
+// Check for bad configuration or something else went wrong
+if (empty($survey) || empty($user))
+    die("Unable to authenticate user. Please contact your REDCap administrator.");
+
+// Build hash and redirect to survey
+$hash = $module->makeHash($survey, $user, $time);
+$base = "http" . (empty($_SERVER["HTTPS"]) ? "" : "s") . "://" . $_SERVER["HTTP_HOST"];
+
+header("Location: $base/surveys/?s=$survey&auth=$hash");
